aefd272a365b2eafd6379644e216d217fb213ecf
qqth <worl4ck@proton.me>
2023-10-24 19:24:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+    TOKEN = config["token"]
+    PREFIX = config["prefix"]
 
-def add_link(link):
-    with open("links.txt", "a") as file:
-        file.write(link + "\n")
+bot = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
 
-def remove_link(link):
+def is_link_duplicate(link):
     with open("links.txt", "r") as file:
-        lines = file.readlines()
-    
-    with open("links.txt", "w") as file:
-        for line in lines:
-            if line.strip() != link:
-                file.write(line)
+        return link in file.read()
 
-def count_links():
-    with open("links.txt", "r") as file:
-        lines = file.readlines()
-    return len(lines)
+async def send_embed(ctx, title, description, color=discord.Color.blue()):
+    embed = discord.Embed(title=title, description=description, color=color)
+    await ctx.send(embed=embed)
 
 @bot.event
 async def on_ready():
@@ -27,17 +22,36 @@ async def on_ready():
 
 @bot.command()
 async def add(ctx, link):
-    add_link(link)
-    await ctx.send(f"Added {link} to links.txt")
+    if is_link_duplicate(link):
+        await send_embed(ctx, "Duplicate Link", f"{link} is already in links.txt.", discord.Color.red())
+    else:
+        add_link(link)
+        await send_embed(ctx, "Link Added", f"Added {link} to links.txt.", discord.Color.green())
 
 @bot.command()
 async def remove(ctx, link):
-    remove_link(link)
-    await ctx.send(f"Removed {link} from links.txt")
+    if is_link_duplicate(link):
+        remove_link(link)
+        await send_embed(ctx, "Link Removed", f"Removed {link} from links.txt.", discord.Color.green())
+    else:
+        await send_embed(ctx, "Link Not Found", f"{link} not found in links.txt.", discord.Color.red())
 
 @bot.command()
 async def link_count(ctx):
     count = count_links()
-    await ctx.send(f"Number of links in links.txt: {count}")
+    await send_embed(ctx, "Link Count", f"Number of links in links.txt: {count}", discord.Color.blue())
 
-bot.run("YOUR_BOT_TOKEN")
+@bot.command()
+async def purge(ctx):
+    try:
+        with open("links.txt", "w") as file:
+            file.write("")
+        await send_embed(ctx, "Purge Complete", "All links removed from links.txt.", discord.Color.green())
+    except Exception as e:
+        await send_embed(ctx, "Purge Failed", f"An error occurred: {e}", discord.Color.red())
+
+@bot.command()
+async def get_links(ctx):
+    await ctx.send(file=discord.File("links.txt"))
+    
+bot.run(TOKEN)
